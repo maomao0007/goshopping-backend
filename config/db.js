108123c4b5431f8b2db120ebed5277d31b6765a4
maomao0007 <skylifepic07@gmail.com>
2025-02-10 14:59:35 +0800

    feat: revise database configuration for production and development environments

--- config/db.js
+++ config/db.js
@@ -1,16 +1,22 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  ssl: {
-    require: true,
-    rejectUnauthorized: false,
-  },
-});
+const getDbConfig = () => {
+  if (process.env.NODE_ENV === "production") {
+    return {
+      connectionString: process.env.INTERNAL_DATABASE_URL,
+      ssl: { require: true, rejectUnauthorized: false },
+    };
+  } else {
+    return {
+      user: process.env.DB_USER,
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT,
+    };
+  }
+};
+const pool = new Pool(getDbConfig());
 
 pool.query("SELECT NOW()", (err, res) => {
   if (err) {
